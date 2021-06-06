97d3b5bfe95322e052ed45013baea4c838c14407
Mohamed Mohey Awnallah <egydeveloper16@gmail.com>
2021-06-06 06:35:24 +0200

    From New Branch

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main{
 public static void main(String[] args) {
     System.out.println("Hello World I'm Now Using Java");
     System.out.println("Hello Again and Again Until the Final Soul");
+    System.out.println("Hello World From New Branch");
 }
 }
