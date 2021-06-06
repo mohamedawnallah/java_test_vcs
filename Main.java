c8a4b27d670a4ac61340d19c3c953f9647e67731
Mohamed Awnallah <69568555+mohamedmohey2352@users.noreply.github.com>
2021-06-06 06:06:46 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
 
 public static void main(String[] args) {
     System.out.println("Hello World I'm Now Using Java");
+    System.out.println("Hello Again and Again Until the Final Soul");
 }
 }
